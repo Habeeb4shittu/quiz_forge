34871a3d10d48953834e42bf7e15f66d1dff834a
Habeeb Shittu <habeeb4shittu@gmail.com>
2025-08-30 09:43:49 +0100

    build error fix

--- src/app/api/quiz/get/[userId]/route.ts
+++ src/app/api/quiz/get/[userId]/route.ts
@@ -3,39 +3,31 @@ import Quiz from "@/models/Quiz";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 
-// Minimal types
-interface RouteParams {
-  params: { userId: string };
-}
-
 export async function GET(
   request: Request,
-  { params }: RouteParams
+  { params }: { params: { userId: string } }
 ): Promise<NextResponse> {
   try {
     await dbConnect();
     const { userId } = params;
-    console.log(params);
 
-    // if (!mongoose.Types.ObjectId.isValid(id)) {
-    //   return NextResponse.json({
-    //     message: "Invalid user ID",
-    //     status: "error",
-    //   });
-    // }
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return NextResponse.json(
+        { message: "Invalid user ID", status: "error" },
+        { status: 400 }
+      );
+    }
 
     const quizzes = await Quiz.find({
       author_id: new mongoose.Types.ObjectId(userId),
-    }).sort({
-      title: 1,
-    });
+    }).sort({ title: 1 });
 
     return NextResponse.json(quizzes, { status: 200 });
   } catch (error: unknown) {
     console.error("Error fetching quizzes:", error);
-    return NextResponse.json({
-      message: "Failed to fetch quizzes",
-      status: "error",
-    });
+    return NextResponse.json(
+      { message: "Failed to fetch quizzes", status: "error" },
+      { status: 500 }
+    );
   }
 }
